a43f77745c105e05ff6a6f21ceb229338639f6f7
eduardobritoc <leduardobritoc@gmail.com>
2023-07-13 21:04:07 -0300

    commands and channels

--- server_thread.c
+++ server_thread.c
@@ -7,99 +7,194 @@
 #include <pthread.h>
 
 #define MAX_CLIENTS 10
+#define MAX_CHANNELS 10
 #define HEADER_SIZE 52
 #define MESSAGE_SIZE 32
 #define BUFFER_SIZE HEADER_SIZE+MESSAGE_SIZE
 
-int client_count = 0;
-// usar uma struct dps?
-int client_sockets[MAX_CLIENTS];
-char client_nicknames[MAX_CLIENTS][20];
-pthread_t client_threads[MAX_CLIENTS];
+struct client{
+    int socket;
+    pthread_t thread; // talvez nao precise
+    char nickname[50];
+    char muted;
+    struct channel *channel;
+    struct sockaddr_in address;
+};
+
+struct channel{
+    char name[50];
+    struct client *admin;
+};
+
+int client_count, channel_count = 0;
+struct client clients[MAX_CLIENTS];
+struct channel channels[MAX_CHANNELS];
 pthread_mutex_t client_mutex;
 
-void broadcast(char *message, int current_client) {
+void broadcast(char *message, struct channel *channel) {
     pthread_mutex_lock(&client_mutex);
     for (int i = 0; i < client_count; i++) {
-        send(client_sockets[i], message, strlen(message), 0);
+        if (channel == clients[i].channel)
+            send(clients[i].socket, message, strlen(message), 0);
     }
     pthread_mutex_unlock(&client_mutex);
 }
 
-void close_socket(int sock){
+void close_socket(int socket){
     pthread_mutex_lock(&client_mutex);
     for (int i = 0; i < client_count; i++) {
-        if (client_sockets[i] == sock) {
-            client_sockets[i] = client_sockets[client_count - 1];
-            strcpy(client_nicknames[client_count - 1], client_nicknames[i]);
+        if (clients[i].socket == socket) {
+            clients[i] = clients[client_count - 1];
             break;
         }
     }
     client_count--;
     pthread_mutex_unlock(&client_mutex);
-    close(sock);
+    close(socket);
     pthread_exit(NULL);
 }
 
-void handle_client(void *client_socket) {
-    int sock = *(int *)client_socket;
+struct client *find_client(char* nickname){
+    for (int i = 0; i < client_count; i++){
+        if (strcmp(nickname, clients[i].nickname) == 0){
+            printf("usuario encontrado: %s\n", clients[i].nickname);
+            fflush(stdout);
+            return &clients[i];
+        }
+    }
+    return NULL;
+}
+
+void handle_client(void *client) {
+    struct client *current_client = (struct client *)client; 
     char buffer[BUFFER_SIZE];
 
     while (1) {
         memset(buffer, 0, BUFFER_SIZE);
-        int read_size = recv(sock, buffer, BUFFER_SIZE, 0);
+        int read_size = recv(current_client->socket, buffer, BUFFER_SIZE, 0);
         if (read_size <= 0) {
-            close_socket(sock);
-        } else {
-            if (strcmp(buffer, "/ping") == 0){
-                send(sock, "pong", 6, 0);
-            }
-            else if (strncmp(buffer, "/nickname ", 10) == 0){
-                printf("buffer: '%s'\n", buffer);
-                for (int i = 0; i < client_count; i++){
-                    if (client_sockets[i] == sock){
-                        char newNickname[20];
-                        strncpy(newNickname, &buffer[10], strlen(buffer) - 10);
-                        newNickname[strlen(buffer) - 10] = '\0'; 
-                        printf("novo apelido: %s\n", newNickname);
-                        strcpy(client_nicknames[i], newNickname);
-                        break;
-                    }
-                }
-            }
-            else if (strncmp(buffer, "/kick ", 6) == 0){
-                printf("buffer: '%s'\n", buffer);
-                for (int i = 0; i < client_count; i++){
-                    if (client_sockets[i] == sock){
-                        char nickname[20];
-                        strncpy(nickname, &buffer[6], strlen(buffer) - 6);
-                        nickname[strlen(buffer) - 6] = '\0'; 
-                        printf("apelido lido: '%s'\n", nickname);
-                        break;
-                    }
-                }
-            }
-            else{   
-                // colocar apelido
-                char message[HEADER_SIZE + MESSAGE_SIZE];
-                for (int i = 0; i < client_count; i++){
-                    if (client_sockets[i] == sock){
-                        strcpy(message, client_nicknames[i]);
-                        break;
-                    }
-                }
-                
-                strcat(message, ": ");
-                strcat(message, buffer);
+            close_socket(current_client->socket);
+        } 
+        else if (current_client->muted){
+            printf("usuario mutado\n");
+            fflush(stdout);
+        }
+        // comandos para todos os usuarios
+        else if (strcmp(buffer, "/ping") == 0){
+            send(current_client->socket, "pong", 5, 0);
+        }
+        else if (strncmp(buffer, "/nickname ", 10) == 0){
+            printf("Comando /nickname chamado\n");
+            strncpy(current_client->nickname, &buffer[10], strlen(buffer)-10);
+            current_client->nickname[strlen(buffer)-10] = '\0'; //talvez tenha que limpar memoria
+            send(current_client->socket, "apelido ok", 10, 0);
+        }
+        else if (strncmp(buffer, "/join ", 6) == 0){
+            char channelName[50];
+            strncpy(channelName, &buffer[6], strlen(buffer)-6);
+            channelName[strlen(buffer)-6] = '\0'; //talvez tenha que limpar memoria
 
-                // enviar
-                broadcast(message, sock);
+            int exists = 0;
+            for (int i = 0; i < channel_count; i++){
+                if (strcmp(channels[i].name, channelName) == 0){
+                    current_client->channel = &channels[i];
+                    exists = 1;
+                    break;
+                }
             }
+            if (!exists){
+                strcpy(channels[channel_count].name, channelName);
+                channels[channel_count].admin = current_client;
+                current_client->channel = &channels[channel_count];
+                channel_count++;
+            }
+        }
+        // comandos para administradores
+        else if (strncmp(buffer, "/kick ", 6) == 0 && current_client->channel->admin == current_client){
+            printf("comando /kick\n");
+            fflush(stdout);
+
+            char nickname[50];
+            strncpy(nickname, &buffer[6], strlen(buffer)-6);
+            nickname[strlen(buffer)-6] = '\0'; //talvez tenha que limpar memoria
+
+            struct client *client = find_client(nickname);
+            if (client){
+                close_socket(client->socket);
+                //free talvez
+            }
+        }
+        else if (strncmp(buffer, "/mute ", 6) == 0 && current_client->channel->admin == current_client){
+            printf("comando /mute\n");
+            fflush(stdout);
+
+            char nickname[50];
+            strncpy(nickname, &buffer[6], strlen(buffer)-6);
+            nickname[strlen(buffer)-6] = '\0'; //talvez tenha que limpar memoria
+
+            struct client *client = find_client(nickname);
+            
+            if (client){
+                client->muted = 1;
+            }
+        }
+        else if (strncmp(buffer, "/unmute ", 8) == 0 && current_client->channel->admin == current_client){
+            printf("comando /unmute\n");
+            fflush(stdout);
+
+            char nickname[50];
+            strncpy(nickname, &buffer[8], strlen(buffer)-8);
+            nickname[strlen(buffer)-8] = '\0'; //talvez tenha que limpar memoria
+            
+            struct client *client = find_client(nickname);
+            if (client){
+                client->muted = 0;
+            }
+        }
+        else if (strncmp(buffer, "/whois ", 7) == 0 && current_client->channel->admin == current_client){
+            printf("comando /WHOIS\n");
+            fflush(stdout);
+
+            char nickname[50];
+            strncpy(nickname, &buffer[7], strlen(buffer)-7);
+            nickname[strlen(buffer)-7] = '\0'; //talvez tenha que limpar memoria
+
+            struct client *client = find_client(nickname);
+            if (client){
+                printf("ip: %s\n", inet_ntoa(client->address.sin_addr));
+                fflush(stdout);
+            }
+            
+
+            /* algo nessa vibe eu acho
+            printf("Novo cliente conectado. Socket: %d, IP: %s, Porta: %d\n",
+            client_socket,
+            inet_ntoa(client_address.sin_addr),
+            ntohs(client_address.sin_port));
+            */
+
+            continue;
+        }
+        // mensagem normal
+        else{   
+            printf("mensagem normal\n");
+            fflush(stdout);
+            // colocar apelido
+            char message[HEADER_SIZE + MESSAGE_SIZE];
+            strcpy(message, current_client->nickname);
+            strcat(message, ": ");
+            strcat(message, buffer);
+
+            // enviar
+            broadcast(message, current_client->channel);
         }
     }
 }
 
 int main() {
+    strcpy(channels[0].name, "canal1");
+    channel_count = 1;
+
     int server_socket, client_socket;
     struct sockaddr_in server_address, client_address;
     socklen_t client_address_length;
@@ -154,14 +249,15 @@ int main() {
 
         // Adicionar cliente à lista
         pthread_mutex_lock(&client_mutex);
-        client_sockets[client_count] = client_socket;
-        strcpy(client_nicknames[client_count], "teste");
-        printf("apelido definido: %s\n", client_nicknames[client_count]);
+        clients[client_count].socket = client_socket;
+        strcpy(clients[client_count].nickname, "teste");
+        clients[client_count].channel = &channels[0];
+        clients[client_count].address = client_address;
         client_count++;
         pthread_mutex_unlock(&client_mutex);
 
         // Criar thread para lidar com o cliente
-        pthread_create(&client_threads[client_count - 1], NULL, (void *)handle_client, &client_socket);
+        pthread_create(&clients[client_count - 1].thread, NULL, (void *)handle_client, &clients[client_count-1]);
 
         printf("Novo cliente conectado. Socket: %d, IP: %s, Porta: %d\n",
             client_socket,
